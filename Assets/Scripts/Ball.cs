7986b7493adc7b88862054815e7ca784c0194f35
TalTsadikov <taltsadikov@gmail.com>
2023-07-18 21:54:07 +0300

    ahfha

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,17 +4,66 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    public bool _gotHit = false;
+    //public bool _gotHit = false;
+    //
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.transform.tag == "Bat")
+    //    {
+    //        _gotHit = true;
+    //        Debug.Log(_gotHit);
+    //        Bat bat = other.GetComponent<Bat>();
+    //        Vector3 hitDirrection = this.transform.position - bat.transform.position;
+    //        transform.position -= hitDirrection.normalized * bat._currentForce * Time.deltaTime;
+    //    }
+    //}
+
+    public bool _gotHit { get; private set; } = false;
+    private Vector3 _hitDirection;
+    private float _hitForce;
+
+    private Rigidbody _ballRigidbody;
+
+    private void Start()
+    {
+        _ballRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (_gotHit)
+        {
+            // Apply hit force to the ball's rigidbody
+            _ballRigidbody.velocity = _hitDirection.normalized * _hitForce;
+        }
+    }
+
+    public void ApplyHitForce(Vector3 direction, float force)
+    {
+        _hitDirection = direction;
+        _hitForce = force;
+        _gotHit = true;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.transform.tag == "Bat")
+        if (other.transform.CompareTag("Bat"))
         {
-            _gotHit = true;
-            Debug.Log(_gotHit);
+            // Ball collided with the bat, apply hit force
             Bat bat = other.GetComponent<Bat>();
-            Vector3 hitDirrection = this.transform.position - bat.transform.position;
-            transform.position -= hitDirrection.normalized * bat._currentForce * Time.deltaTime;
+            Vector3 hitDirection = transform.position - bat.transform.position;
+            ApplyHitForce(hitDirection, bat._currentForce);
         }
     }
+
+    public bool IsHit()
+    {
+        return _gotHit;
+    }
+
+    public void ResetHit()
+    {
+        _gotHit = false;
+    }
+
 }
