698644a5efa8cea8c3a095c0c74fb2456e943ab2
TalTsadikov <taltsadikov@gmail.com>
2023-07-19 13:47:15 +0300

    a

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,66 +4,18 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    //public bool _gotHit = false;
-    //
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.transform.tag == "Bat")
-    //    {
-    //        _gotHit = true;
-    //        Debug.Log(_gotHit);
-    //        Bat bat = other.GetComponent<Bat>();
-    //        Vector3 hitDirrection = this.transform.position - bat.transform.position;
-    //        transform.position -= hitDirrection.normalized * bat._currentForce * Time.deltaTime;
-    //    }
-    //}
-
-    public bool _gotHit { get; private set; } = false;
-    private Vector3 _hitDirection;
-    private float _hitForce;
-
-    private Rigidbody _ballRigidbody;
-
-    private void Start()
-    {
-        _ballRigidbody = GetComponent<Rigidbody>();
-    }
-
-    private void Update()
-    {
-        if (_gotHit)
-        {
-            // Apply hit force to the ball's rigidbody
-            _ballRigidbody.velocity = _hitDirection.normalized * _hitForce;
-        }
-    }
-
-    public void ApplyHitForce(Vector3 direction, float force)
-    {
-        _hitDirection = direction;
-        _hitForce = force;
-        _gotHit = true;
-    }
-
+    public bool _gotHit = false;
+    
     private void OnTriggerEnter(Collider other)
     {
-        if (other.transform.CompareTag("Bat"))
+        if (other.transform.tag == "Bat")
         {
-            // Ball collided with the bat, apply hit force
+            _gotHit = true;
+            Debug.Log(_gotHit);
             Bat bat = other.GetComponent<Bat>();
-            Vector3 hitDirection = transform.position - bat.transform.position;
-            ApplyHitForce(hitDirection, bat._currentForce);
+            Vector3 hitDirrection = this.transform.position - bat.transform.position;
+            transform.position -= hitDirrection.normalized * bat._currentForce * Time.deltaTime;
         }
-    }
-
-    public bool IsHit()
-    {
-        return _gotHit;
-    }
-
-    public void ResetHit()
-    {
-        _gotHit = false;
     }
 
 }
--- Assets/Scripts/PitchingMachine.cs
+++ Assets/Scripts/PitchingMachine.cs
@@ -5,109 +5,16 @@ using UnityEngine.UI;
 
 public class PitchingMachine : MonoBehaviour
 {
-    // [Header("Ball Variables")]
-    // [SerializeField] GameObject _prefab;
-    // [SerializeField] Transform _spawnPoint;
-    // Rigidbody _ballRigidbody;
-    // private Vector3 _initialPosition;
-    //
-    // [Header("Throw Variables")]
-    // [SerializeField] float _throwForce;
-    // Vector3 _throwDirection;
-    // bool _isThrowing = false;
-    //
-    // [Header("Slider Variables")]
-    // [SerializeField] Slider _throwForceSlider;
-    // [SerializeField] private float _minSliderValue = 1f;
-    // [SerializeField] private float _maxSliderValue = 100f;
-    // [SerializeField] private float growthSpeed = 20f;
-    // private float _currentThrowPower = 0f;
-    // private bool _isSliderIncreasing = true;
-    //
-    // [SerializeField] Ball _ball;
-    //
-    //void Start()
-    //{
-    //    _ballRigidbody = GetComponent<Rigidbody>();
-    //    _ball = GetComponent<Ball>();
-    //    _ballRigidbody.useGravity = false;
-    //    _initialPosition = transform.position;
-    //    _throwDirection = Vector3.back;
-    //    _currentThrowPower = _minSliderValue;
-    //    _throwForceSlider.value = _currentThrowPower;
-    //}
-    //
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    if (Input.GetKeyDown(KeyCode.Space) && !_isThrowing)
-    //    {
-    //        if (_ball._gotHit == false)
-    //        {
-    //            ThrowBall();
-    //        }
-    //        else
-    //            Debug.Log("not");
-    //    }
-    //
-    //    UpdateSliderValue();
-    //}
-    //
-    //void FixedUpdate()
-    //{
-    //    if (_isThrowing)
-    //    {
-    //        if (_ballRigidbody.velocity.magnitude <= 0.01f)
-    //        {
-    //            _ballRigidbody.velocity = Vector3.zero;
-    //            _ballRigidbody.angularVelocity = Vector3.zero;
-    //            _isThrowing = false;
-    //        }
-    //    }
-    //}
-    //
-    //void ThrowBall()
-    //{
-    //    _ballRigidbody.velocity = _throwDirection * _currentThrowPower;
-    //    _ballRigidbody.useGravity = true;
-    //    _isThrowing = true;
-    //}
-    //
-    //void UpdateSliderValue()
-    //{
-    //    if (!_isThrowing)
-    //    {
-    //        if (_isSliderIncreasing)
-    //        {
-    //            _currentThrowPower += growthSpeed * Time.deltaTime;
-    //            if (_currentThrowPower >= _maxSliderValue)
-    //            {
-    //                _currentThrowPower = _maxSliderValue;
-    //                _isSliderIncreasing = false;
-    //            }
-    //        }
-    //        else
-    //        {
-    //            _currentThrowPower -= growthSpeed * Time.deltaTime;
-    //            if (_currentThrowPower <= _minSliderValue)
-    //            {
-    //                _currentThrowPower = _minSliderValue;
-    //                _isSliderIncreasing = true;
-    //            }
-    //        }
-    //    }
-    //
-    //    _throwForceSlider.value = _currentThrowPower;
-    //}
     [Header("Ball Variables")]
-    [SerializeField] Transform _ballTransform;
-    private Ball _ball;
-    private Rigidbody _ballRigidbody;
-    private bool _isThrowing = false;
+    [SerializeField] GameObject _prefab;
+    [SerializeField] Transform _spawnPoint;
+    Rigidbody _ballRigidbody;
+    private Vector3 _initialPosition;
 
     [Header("Throw Variables")]
     [SerializeField] float _throwForce;
     Vector3 _throwDirection;
+    bool _isThrowing = false;
 
     [Header("Slider Variables")]
     [SerializeField] Slider _throwForceSlider;
@@ -117,21 +24,17 @@ public class PitchingMachine : MonoBehaviour
     private float _currentThrowPower = 0f;
     private bool _isSliderIncreasing = true;
 
-    private Vector3 _initialPosition;
-    private bool _gravityEnabled = false;
+    [SerializeField] Ball _ball;
 
     void Start()
     {
+        _ballRigidbody = GetComponent<Rigidbody>();
+        _ball = GetComponent<Ball>();
+        _ballRigidbody.useGravity = false;
+        _initialPosition = transform.position;
         _throwDirection = Vector3.back;
         _currentThrowPower = _minSliderValue;
         _throwForceSlider.value = _currentThrowPower;
-
-        _ball = _ballTransform.GetComponent<Ball>();
-        _ballRigidbody = _ballTransform.GetComponent<Rigidbody>();
-
-        _initialPosition = _ballTransform.position; // Store the initial position
-
-        _ballRigidbody.useGravity = false; // Disable gravity initially
     }
 
     // Update is called once per frame
@@ -139,33 +42,35 @@ public class PitchingMachine : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) && !_isThrowing)
         {
-            if (!_ball.IsHit())
+            if (_ball._gotHit == false)
             {
                 ThrowBall();
-                _gravityEnabled = true; // Enable gravity when the ball is thrown
             }
+            else
+                Debug.Log("not");
         }
 
         UpdateSliderValue();
+    }
 
+    void FixedUpdate()
+    {
         if (_isThrowing)
         {
-            Vector3 movement = _throwDirection * _currentThrowPower * Time.deltaTime;
-            _ballTransform.position += movement;
-
-            if (_gravityEnabled)
+            if (_ballRigidbody.velocity.magnitude <= 0.01f)
             {
-                // Apply gravity
-                Vector3 gravityMovement = Physics.gravity * Time.deltaTime;
-                _ballTransform.position += gravityMovement;
+                _ballRigidbody.velocity = Vector3.zero;
+                _ballRigidbody.angularVelocity = Vector3.zero;
+                _isThrowing = false;
             }
         }
     }
 
     void ThrowBall()
     {
+        _ballRigidbody.velocity = _throwDirection * _currentThrowPower;
+        _ballRigidbody.useGravity = true;
         _isThrowing = true;
-        _ball.ApplyHitForce(_throwDirection, _currentThrowPower);
     }
 
     void UpdateSliderValue()
@@ -193,13 +98,5 @@ public class PitchingMachine : MonoBehaviour
         }
 
         _throwForceSlider.value = _currentThrowPower;
-    }
-
-    public void ResetBall()
-    {
-        _isThrowing = false;
-        _gravityEnabled = false; // Disable gravity
-        _ball.ResetHit();
-        _ballTransform.position = _initialPosition; // Reset ball position
     }
 }
